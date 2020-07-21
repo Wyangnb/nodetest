213475deb3be2e07ee497270288b898aa47ec534
Wy <1076861800@qq.com>
2020-07-21 22:50:19 +0800

    ss

--- nodeTest/app.js
+++ nodeTest/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./public/'))
+
 app.get('/', (req, res) => {
-    res.render('index.html')
+    res.send('hello')
 })
 app.listen('3001', (err) => {
     console.log('启动成功')
